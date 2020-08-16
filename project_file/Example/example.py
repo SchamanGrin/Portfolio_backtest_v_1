f0cf4298d9a29aa17914913f8d13a232d5b44359
grin <i_grin@mail.ru>
2020-08-16 11:25:02 +0300

    TWRR in perfomance.py

--- project_file/Example/example.py
+++ project_file/Example/example.py
@@ -5,7 +5,7 @@ data = pd.read_csv(
     'symbol/SPY.csv', header=0, index_col=0,
                 names=['timestamp', 'open', 'low', 'high', 'close', 'volume'], parse_dates=['timestamp']
 )
-start_date = pd.to_datetime('2020-03-08')
+start_date = pd.to_datetime('2010-01-01')
 data.reindex(pd.to_datetime(data.index, '%Y%m%d'))
 
 data_cashflow = pd.DataFrame(data.loc[data.index >= start_date]['close'][::-1])
@@ -13,6 +13,8 @@ data_cashflow.loc[:,'cashflow'] = 0.*len(data_cashflow.index)
 data_cashflow['cashflow'][0]=-data_cashflow['close'][0]
 data_cashflow['cashflow'][-1]=data_cashflow['close'][-1]
 data_cashflow.rename(columns={'close':'total'}, inplace=True)
+
+data_cashflow['year'] = data_cashflow.index.year
 
 result = twrr(data_cashflow)
 
