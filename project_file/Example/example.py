68c2534faf53cca7f3b1b258c72b3e15c23f810e
grin <i_grin@mail.ru>
2021-01-04 20:05:52 +0300

    create_perform in performance.py

--- project_file/Example/example.py
+++ project_file/Example/example.py
@@ -3,24 +3,21 @@ from pathlib import Path
 
 def read_csv(path):
     return pd.read_csv(path, header=0, index_col=0,
-                     names=['timestamp', 'close', 'cf', 'count','cf and total end', 'total', 'result'], parse_dates=['timestamp'], sep=';',
+                     names=['timestamp', 'total', 'cf', 'twrr'], parse_dates=['timestamp'], sep=';',
                      dayfirst=True, decimal=',')
 
-path = Path('testcase') / 'positive_far_many_cf.csv'
+path = Path('testcase') / 'twrr_far_many_cf.csv'
 
 data = read_csv(path)
 
-cf_data = pd.DataFrame(data[['cf and total end']])
+'''cf_data = pd.DataFrame(data[['cf and total end']])
 cf_data = cf_data['cf and total end'].reset_index()
 cf_data['timestamp'] = cf_data['timestamp'].astype('datetime64[ns]')
-arr_cf = cf_data[['timestamp', 'cf and total end']].to_numpy()
+arr_cf = cf_data[['timestamp', 'cf and total end']].to_numpy()'''
 
 data_rename = data[['total', 'cf']].copy()
 data_rename.rename(columns={'cf':'cashflow'}, inplace=True)
 #dict_data = dict(zip(data.index,data['cf']))
 
-revenue = create_return(data_rename, ['twrr'])['data']
-cr = create_sharpe_ratio(revenue['twrr'])
-cd = create_drawdowns(revenue['twrr'])
-print(cr)
-print(cd)
+revenue = create_return(data_rename, ['twrr'])
+print(revenue['twrr'])
